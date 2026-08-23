d3d4043976c2eb25962bfb6a33cee2ae9eacaf4f
Pascal <pas111cal3@gmail.com>
2026-08-23 13:54:44 +0100

    added smooter scrolling using lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-
+import SmoothScroll from "./Components/animations/SmoothScroll";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "@/Components/NavBar";
 import WhatsAppIcon from "./Components/WhatsAppIcon";
@@ -14,10 +14,12 @@ const ApplyPage = lazy(() => import("./Pages/ApplyPage/ApplyPage"));
 function App() {
   return (
     <>
+      <SmoothScroll />
       <NavBar></NavBar>
-      <WhatsAppIcon/>
-      <Suspense fallback = { <PageLoader/>}>
-        <Routes >
+
+      <WhatsAppIcon />
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
           <Route index element={<HomePage />} />
           <Route path="/programme/:slug" element={<ProgrammesPage />} />
           <Route path="/apply" element={<ApplyPage />} />
