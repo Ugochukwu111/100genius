cbbe765a4e33ef9fbe1c5aaeb00476d75020f32a
Pascal <pas111cal3@gmail.com>
2026-08-26 23:05:56 +0100

    feat: add join community popup

--- src/Components/AppLayout.jsx
+++ src/Components/AppLayout.jsx
@@ -1,17 +1,47 @@
-import Header from "./Header";
+import { useEffect, useState } from "react";
 import Footer from "./Footer";
-import { Children } from "react";
+import CommunityJoinModal from "./CommunityModal";
 
-export default function AppLayout({header, children }) {
+export default function AppLayout({ header, children }) {
+  const [showCommunityModal, setShowCommunityModal] = useState(false);
+
+  useEffect(() => {
+    const hasSeenCommunityModal = localStorage.getItem(
+      "100genius-community-modal-seen"
+    );
+
+    if (hasSeenCommunityModal) return;
+
+    const timer = setTimeout(() => {
+      setShowCommunityModal(true);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleCloseCommunityModal = () => {
+    setShowCommunityModal(false);
+
+    localStorage.setItem(
+      "100genius-community-modal-seen",
+      "true"
+    );
+  };
+
   return (
-    <div className="flex flex-col flex-1">
-         {header}
+    <div className="flex min-h-screen flex-col">
+      {header}
 
       <main className="flex-1">
         {children}
       </main>
 
       <Footer />
+
+      <CommunityJoinModal
+        open={showCommunityModal}
+        onClose={handleCloseCommunityModal}
+      />
     </div>
   );
 }
